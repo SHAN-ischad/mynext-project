b2d425719b6655a81413eae1a63a81860ab5fc1e
Iago Vieira Carvalho <iagovelox@gmail.com>
2026-01-03 19:04:02 -0300

    interface 100% concluída

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,29 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "../../components/ui/button";
+import { Card, } from "@/components/ui/card";
+import { Separator } from "@radix-ui/react-separator";
+import { Plus } from 'lucide-react';
+import { Badge } from "@/components/ui/badge";
+
 export default function app() {
   return (
-    <main className="h-screen flex items-center flex-col justify-center">
-      <h1 className="text-red-300 text-[20px]">Botão maneiro</h1>
+    <main className="h-screen  flex items-center flex-col justify-center">
 
-      <div className=" flex flex-row gap-1">
-        <Input className=" " placeholder="Email" />
-        <Button >o botão</Button>
-      </div>
+      <Card className="mt-5 w-1/2 ">
+
+        <div className=" flex flex-row justify-center gap-1">
+          <Input className="w-[40%]" placeholder="Adicionar tarefa" />
+          <Button className="bg-blue-500" ><Plus />Adicionar</Button>
+        </div>
+
+        <Separator className="" orientation="horizontal" />
+
+        <div className="flex flex-row items-start">
+          <Badge />
+
+        </div>
+
+      </Card>
 
     </main>
   );
